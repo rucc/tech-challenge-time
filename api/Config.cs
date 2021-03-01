d0293a09ab736e10c991be1d6162dd95143b56a4
hari <lharangozo@feastfox.com>
2021-03-01 18:24:25 +0100

    proj init

---
+++ api/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pentoTrack
+{
+	public class Config
+	{
+		public string TrackersJsonFileLocation = "./trackers.json";
+		public string ContentRoot { get; set; }
+	}
+}
---
+++ api/Controllers/UiController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pentoTrack.Controllers
+{
+	[Route("ui")]
+	[ApiController]
+	public class UiController : ControllerBase
+	{
+		readonly Config m_cfg;
+
+		public UiController(Config cfg)
+		{
+			m_cfg = cfg;
+		}
+
+		[HttpGet]
+		public ActionResult Get()
+		{
+			return Content(System.IO.File.ReadAllText(Path.Combine(m_cfg.ContentRoot, "index.html")), "text/html", Encoding.UTF8);
+		}
+	}
+}
